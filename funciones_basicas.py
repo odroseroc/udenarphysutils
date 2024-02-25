485b8debf0a97f1cb5a3591fb928864f77812e54
odroseroc <odroseroc@gmail.com>
2024-02-25 16:48:37 -0500

    Nueva estructura del proyecto
    Dividí udenarphysutils.py en diferentes submódulos que contienen diferentes partes del proyecto:
    
    	funciones_basicas.py contiene funciones básicas que serán usadas por todos los módulos del proyecto y que no requieren más que el módulo math de python para funcionar.
    	EValue.py contiene la clase EValue, que anteriormente se llamaba ExperimentalValue y las funciones asociadas a ella.
    	udenarphysutils.py es el módulo principal que puede ejecutarse en la consola interactiva de Python. Unicamente importa todo de los otros submódulos y permite que el usuario haga uso de todas las funcionalidades.
    
    Cambié los nombres de las funciones, variables y documentación a español. Considero que será más útil para los estudiantes de la Universidad de Nariño tener todo el módulo escrito en este idioma.

---
+++ funciones_basicas.py
@@ -0,0 +1,8 @@
+import math
+
+def isnumber(number):
+	return isinstance(number, int) or isinstance(number, float)
+
+
+if __name__ == "__main__":
+	print("Ejecutando basic_functions.py del módulo udenarphys\n")
